bfb5387252674fc6e57989bff92ced2ac0caec29
aviva <aviva@jfrog.com>
2017-04-03 14:17:05 +0300

    groovyTest first commit

---
+++ myGroovyTest/src/Main.groovy
@@ -0,0 +1,14 @@
+/**
+ * Created by aviva on 03/04/2017.
+ *
+ */
+class Main {
+
+     static void main(String[] args) {
+
+        MyGroovyClass mgc = new MyGroovyClass()
+        mgc.printNum()
+    }
+
+
+}
---
+++ myGroovyTest/src/MyGroovyClass.groovy
@@ -0,0 +1,15 @@
+/**
+ * Created by aviva on 03/04/2017.
+ *
+ */
+
+
+ class MyGroovyClass {
+
+     static  void printNum() {
+
+        def x = 12
+        println x.toString()
+    }
+
+}
